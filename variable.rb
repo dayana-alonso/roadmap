36beff7f0e488fc474c37052fedc82aefeadfabc
Dayana Alonso <dayanaalono83@gmail.com>
2019-11-06 10:05:35 +0000

    roadmap_ruby

---
+++ variable.rb
@@ -0,0 +1,31 @@
+
+composer = 'Mozart'
+puts composer + ' was "da bomb", in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+puts "Hi what is you name? "
+name = gets.chomp
+puts 'Wow! ' + name + ' what a nice name!'
+
+puts "Hi what is you name? "
+name = gets.chomp
+puts ''+ name + ' what a nice name!'
+
+name = 'Dayan Alonso'
+puts 'is your name ' + name + ' really  ' + name + '?'
+
+
+singer = 'John Lenon'
+puts singer + ' is "da bomb", don\'t you agree?.'
+
+leadsinger = 'John Bon Jovi'
+puts 'nah I prefer ' + leadsinger + ', personally.'
+
+puts 'what is your favorite singer?'
+x = gets.chomp
+puts 'really? you\'r favorite singer is '  + x  +  ' ? '
+puts 'are you sure...'
+y = gets.chomp
+puts  ' ' + x  + ' WoW!! is mine too!'
