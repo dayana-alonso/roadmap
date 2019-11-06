be25798dddb094af22c2f2a1e2149abdf4cf07bf
Dayana Alonso <dayanaalono83@gmail.com>
2019-11-06 10:56:30 +0000

    integer_ruby

---
+++ ruby_exercises/integer.rb
@@ -0,0 +1,30 @@
+# number = 1
+# puts number
+
+# a = 1
+# puts a
+
+# large_number = 1
+# puts large_number
+
+# apples = 1
+# puts apples
+# ===============
+# number = 4
+# number = number * 3
+# puts number + 2
+
+# Same as 
+
+# puts 4 * 3 + 2
+# ================
+
+# # Things on the right goes first
+# Ruby will first look at the expression 2 + 3 * 4, 
+# which will result in the number (object) 14. She will then assign the name number to this object
+
+# number = 2 + 3 * 4
+# puts number
+
+number = 4 + 2 * 4
+puts number
