1450774daefc87f3c304c13796f2dbef087b8afd
Dayana Alonso <dayanaalono83@gmail.com>
2019-11-15 12:49:57 +0000

    added main.rb exercise for classes

--- ruby_useful_links.rb
+++ ruby_useful_links.rb
@@ -1,3 +1,6 @@
 ruby useful links
  https://www.tutorialspoint.com/ruby/ruby_classes.htm
  https://www.tutorialspoint.com/ruby/ruby_class_case_study.htm
+
+http://ruby-for-beginners.rubymonstas.org/objects/classes.html
+
