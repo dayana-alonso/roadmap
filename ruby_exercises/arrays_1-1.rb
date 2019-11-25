dc6e0aca694fe8148d99376b586fb32cf7afe298
Dayana Alonso <dayanaalono83@gmail.com>
2019-11-25 13:04:52 +0000

    ruby exercises

---
+++ ruby_exercises/arrays_1-1.rb
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p numbers
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers = numbers.select { |number| number.even? }
+# p numbers
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers = "reverse() method form : #{numbers.reverse()}\n\n"
+# puts numbers
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers = numbers.select { |number| numbers.even? }.reverse
+# numbers.delete(6)
+# p numbers
+
+
+
---
+++ ruby_exercises/hashes_1-1.rb
@@ -0,0 +1,13 @@
+# dictionary = { :one => 'uno', :two => 'dos', :three => 'tres' }
+# puts dictionary [:two]
+
+
+# dictionary = { :one => 'uno', :two => 'dos', :three => 'tres', }
+# dictionary = {:four =>'quattro'}
+# puts dictionary[:four]
+
+# puts "geeks".capitalize() 
+
+# puts "quattro".capitalize()
+
+dictionary = { :one => 'uno', :two => 'dos', :three => 'tres' }
