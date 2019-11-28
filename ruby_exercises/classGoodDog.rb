3ae5a12cccb7011391a36f77701b1d67e81cc075
Dayana Alonso <dayanaalono83@gmail.com>
2019-11-28 11:32:45 +0000

    added replace instance variable

--- ruby_exercises/classGoodDog.rb
+++ ruby_exercises/classGoodDog.rb
@@ -46,20 +46,52 @@
 
 # attr_accessor
 
-class GoodDog
-  attr_accessor :name
+# class GoodDog
+#   attr_accessor :name
 
-  def initialize(name)
-    @name = name
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def speak
+#     "#{@name} says Voff Arf! Voff"
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky")
+# puts sparky.speak
+# puts sparky.name           
+# sparky.name = "Spartacus"
+# puts sparky.name           
+
+# ===============
+
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
   end
 
   def speak
-    "#{@name} says Voff Arf! Voff"
+    "#{name} says arf!"
+  end
+
+  def change_info(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
   end
 end
 
-sparky = GoodDog.new("Sparky")
-puts sparky.speak
-puts sparky.name           
-sparky.name = "Spartacus"
-puts sparky.name           
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
+
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info  
