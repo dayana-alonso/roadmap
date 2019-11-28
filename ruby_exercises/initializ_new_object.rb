64b83c09bfd54e56a5192102a2b949c6187ad6c8
Dayana Alonso <dayanaalono83@gmail.com>
2019-11-28 11:19:42 +0000

    instance method

--- ruby_exercises/initializ_new_object.rb
+++ ruby_exercises/initializ_new_object.rb
@@ -1,7 +1,32 @@
+# class GoodDog
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky")
+
+# puts GoodDog.name
+
+
+# Instance Methods
+
 class GoodDog
-  def initialize
-    puts "This object was initialized!"
-  end
+	def initialize(name)
+		@name = name
+	end
+
+	def speak
+		"Voff Arf! Voff"
+	end
+
+
+	def speak
+		"Voff Arf! Voff"
+	end
 end
 
-sparky = GoodDog.new
+fido = GoodDog.new("fido")
+sparky =GoodDog.new('Sparky')
+
+puts sparky.speak
